862bf92d19fb9a7e645ec325cb0f523610721a0a
EugeneOss <214120507+EugeneOss@users.noreply.github.com>
2025-06-18 19:50:59 +0300

    Сделал через pip

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 pg = st.navigation([
-    st.Page('main.py', title="Анализ акций Nasdaq"),
+    st.Page('pages/main.py', title="Анализ акций Nasdaq"),
     st.Page('pages/first_page.py', title="Анализ чаевых"),
 ])
 
